d2a27f2d9158320ee52919385b0e66cf922ec8e2
ejariasm <ejarisasm@unal.edu.co>
2020-05-01 14:43:05 -0500

    Quemada imagen de perfil y seguidores

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import Vue from 'vue';
 import bootstrapvue from 'bootstrap-vue';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bulma';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import './assets/css/index.scss';
+
 
 Vue.config.productionTip = false;
 
--- src/views/User.vue
+++ src/views/User.vue
@@ -1,8 +1,28 @@
 <template>
   <div class="home">
 
-      <p>6</p>
-      <h6>Followers</h6>
+   <div class="pin-user__profile">
+     <div class="pin-user__header-wrapper">
+       <div class="pin-user__header-left">
+         <h1>Jungkook</h1>
+         <div  class="pin-user__follow">
+           <div>
+             <p>6</p>
+             <h6>Seguidores</h6>
+           </div>
+           <div>
+             <p>6</p>
+             <h6>Siguiendo</h6>
+           </div>
+         </div>
+       </div>
+       <div class="pin-user__header-right">
+         <figure class="image is-128x128">
+          <img src="//i.imgur.com/XTNBAGt.jpg" class="is-rounded" >
+         </figure>
+       </div>
+     </div>
+    </div>
 
   </div>
 
