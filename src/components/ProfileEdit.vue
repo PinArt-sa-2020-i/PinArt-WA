1acc28dd504798c416ff3c46e5280f20b2b20a2d
Elsa Johanna Arias Muñoz <ejariasm@unal.edu.co>
2020-05-07 12:21:21 -0500

    Profile complete
    No sirve el upload profile

--- src/components/ProfileEdit.vue
+++ src/components/ProfileEdit.vue
@@ -1,15 +1,89 @@
 <template id="profile-edit">
-  <section>
-    <h2>Edit profile</h2>
-    <form v-on:submit="updateprofile">
-      <div class="form-group">
-        <label for="edit-name">Name</label>
-        <input class="form-control" id="edit-first-name" v-model="user.firstName" required/>
-      </div>
 
 
-      <button type="submit" class="btn btn-primary">Save</button>
-      <router-link to="'/'" class="btn btn-default">Cancel</router-link>
-    </form>
-  </section>
+      <form v-on:submit.prevent="">
+        <div class="form-group">
+          <input v-model="profile.fechaNacimiento" class="form-control control"
+                 type="date" id="fechaNacimiento"
+                 placeholder="Fecha de nacimiento">
+        </div>
+        <div class="form-group">
+
+          <p>Genero:</p>
+          <input type="radio" id="masculino"  v-model="profile.genero"  value="Masculino">
+          <label for="masculino">Masculino</label><br>
+          <input type="radio" id="femenino"  v-model="profile.genero" value="Femenino">
+          <label for="femenino">Femenino</label><br>
+          <input type="radio" id="otro"  v-model="profile.genero" value="Otro">
+          <label for="otro">Otro</label>
+        </div>
+        <div class="form-group">
+          <input v-model="profile.descripcion" class="form-control control"
+                 type="text" id="descripcion"
+                 placeholder="Descripcion">
+        </div>
+
+        <button  @click="onLoadFoto"  class="btn btn-primary">Cargar foto</button>
+        <button  @click="mutateNow"  class="btn btn-primary">Save</button>
+        <button  @click="onCancel"  class="btn btn-primary">Cancel</button>
+
+      </form>
+
 </template>
+
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+  name: 'ProfileEdit',
+  props: {
+    profile: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+  },
+  methods: {
+    onEdit(data) {
+      if (data) {
+        console.log('profile saved');
+        this.$emit('saved', 'someValue');
+      }
+    },
+    onCancel() {
+      this.$emit('cancelled', 'someValue');
+    },
+    mutateNow() {
+      this.$apollo.mutate({
+        // eslint-disable-next-line global-require
+        mutation: require('../graphql/updateProfile.gql'),
+        variables: {
+          userId: this.profile.userId,
+          countryId: this.profile.countryId,
+          fechaNacimiento: this.profile.fechaNacimiento,
+          genero: this.profile.genero,
+          foto: this.profile.foto,
+          descripcion: this.profile.descripcion,
+        },
+        context: {
+          headers: {
+            Authorization: this.token,
+          },
+        },
+      })
+        .then((results) => {
+          // eslint-disable-next-line no-console
+          console.log(results);
+          this.$emit('saved', results.data.updateProfile);
+        });
+    },
+  },
+  computed: {
+    ...mapState({
+      token: (state) => state.token,
+    }),
+  },
+};
+</script>
